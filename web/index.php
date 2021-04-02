d08365295cdde11ef2a61c44840ed9812bb7d759
IamHere3 <31707856+IamHere3@users.noreply.github.com>
2021-04-02 16:49:20 +0100

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -22,4 +22,23 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
+//database connection
+
+$host = "ec2-3-211-37-117.compute-1.amazonaws.com";
+$dbname = "d81l6hfqatknea";
+$user = "xdirfqqbivnlvo";
+$password = "d2a96e5fc52efc75c15678f4e581aa9783481cf9af49083248ca25834b559a1a";
+$port = "5432";
+
+$dsn = "pgsql:host=$host;dbname=$dbname;user=$user;port=$port;password=$password";
+
+$db = new PDO($dsn);
+
+if($db){
+  echo "Connected <br />".$db;
+}else {
+  echo "Not connected";
+}
+
 $app->run();
+?>
