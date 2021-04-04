a19cd5d5a6bcd9e68e0da4326d98b0cd606b6cee
IamHere3 <31707856+IamHere3@users.noreply.github.com>
2021-04-04 14:38:47 +0100

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -1,5 +1,4 @@
 <?php
-include ('database/databaseconnection.php')?
 require('../vendor/autoload.php');
 
 $app = new Silex\Application();
@@ -15,45 +14,21 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
-//database stuff
-$host = "ec2-3-211-37-117.compute-1.amazonaws.com";
-$user = "xdirfqqbivnlvo";
-$password = "d2a96e5fc52efc75c15678f4e581aa9783481cf9af49083248ca25834b559a1a";
-$dbname = "d81l6hfqatknea";
-$port = "5432";
-
-
-try{
-	$myPDO = new PDO('pgsql:host=localhost;dbname=d81l6hfqatknea', 'postgres', 'secret');
-
-
-  //create a pdo instance
-  $pdo = new PDO($dsn, $user, $password);
-  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) 
-{
-echo 'Connection failed: ' . $e->getMessage();
-}
-
-try 
-{
-	$result = $myPDO->query('SELECT * FROM users');
-	echo $result;
-}
-catch (PDOException $e)
-{
-	echo 'Query Error: ' . $e->getMessage();
-}
-
 // Our web handlers
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
 });
+
+try{
+	$db_connection = pg_connect("host=localhost dbname=d81l6hfqatknea user=postgres password=secret");
+}
+catch (PDOException $e) 
+{
+echo 'Connection failed: ' . $e->getMessage();
+}
+
 
 $app->run();
 ?>
