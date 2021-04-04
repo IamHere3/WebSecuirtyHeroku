c4698ad40d69af5b34b8c02dda6040738fd3f281
IamHere3 <31707856+IamHere3@users.noreply.github.com>
2021-04-04 14:28:27 +0100

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -16,6 +16,28 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 ));
 
 //database stuff
+$host = "ec2-3-211-37-117.compute-1.amazonaws.com";
+$user = "xdirfqqbivnlvo";
+$password = "d2a96e5fc52efc75c15678f4e581aa9783481cf9af49083248ca25834b559a1a";
+$dbname = "d81l6hfqatknea";
+$port = "5432";
+
+
+try{
+	$myPDO = new PDO('pgsql:host=localhost;dbname=d81l6hfqatknea', 'postgres', 'secret');
+
+
+  //create a pdo instance
+  $pdo = new PDO($dsn, $user, $password);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) 
+{
+echo 'Connection failed: ' . $e->getMessage();
+}
+
 try 
 {
 	$result = $myPDO->query('SELECT * FROM users');
